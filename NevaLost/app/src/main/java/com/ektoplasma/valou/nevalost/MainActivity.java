faa3c2b2067fee16567e7f85f25e8014acac5d81
valoulou <valentin.chaigneau@gmail.com>
2016-04-21 18:05:42 +0200

    Le beau commit

--- NevaLost/app/src/main/java/com/ektoplasma/valou/nevalost/MainActivity.java
+++ NevaLost/app/src/main/java/com/ektoplasma/valou/nevalost/MainActivity.java
@@ -2,6 +2,9 @@ package com.ektoplasma.valou.nevalost;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -10,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button ButtonPursuit = (Button)findViewById(R.id.ButtonCreatePursuit);
+        Button ButtonJoin = (Button)findViewById(R.id.ButtonJoinPursuit);
+
+        ButtonPursuit.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                        startActivity(new Intent(MainActivity.this, Creation.class));
+                    }
+                }
+        );
+
+        ButtonPursuit.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                       // startActivity(new Intent(MainActivity.this, JoinActivity.class));
+                    }
+                }
+        );
     }
 }
