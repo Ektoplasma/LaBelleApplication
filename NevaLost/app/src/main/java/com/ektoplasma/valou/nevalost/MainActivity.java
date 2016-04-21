f163be7a32a9a2a1e7104c8ff68da70a24dfca8b
Ektoplasma <acrasidae@gmail.com>
2016-04-21 16:55:30 +0200

    commentaire

--- NevaLost/app/src/main/java/com/ektoplasma/valou/nevalost/MainActivity.java
+++ NevaLost/app/src/main/java/com/ektoplasma/valou/nevalost/MainActivity.java
@@ -4,8 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
+    // le beau commentaire
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
