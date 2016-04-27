f95be768f97ae20f5bd06c785c5434c0b5ad6038
valoulou <valentin.chaigneau@gmail.com>
2016-04-27 21:05:13 +0200

    MAJ_27/04/16

--- NevaLost/app/src/main/java/com/ektoplasma/valou/nevalost/TestMap.java
+++ NevaLost/app/src/main/java/com/ektoplasma/valou/nevalost/TestMap.java
@@ -34,6 +34,13 @@ public class TestMap extends FragmentActivity implements OnMapReadyCallback {
         public void onReceive(Context context, Intent intent) {
             if(intent.getAction().equals(RECEIVE_JSON)) {
                 String serviceJsonString = intent.getStringExtra("json");
+                GetLocalisation malocalisation = new GetLocalisation(getApplicationContext());
+
+                LatLng quelquepart = new LatLng(malocalisation.latitude, malocalisation.longitude);
+                LatLng ailleur = new LatLng(12.80, 3.50);
+                mMap.addMarker(new MarkerOptions().position(quelquepart).title("Le beau marqueur"));
+                mMap.addMarker(new MarkerOptions().position(ailleur).title("Le second marquer"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(quelquepart));
                 //Do something with the string
             }
         }
@@ -83,13 +90,13 @@ public class TestMap extends FragmentActivity implements OnMapReadyCallback {
 
         @Override
         public void onReceive(Context context, Intent intent) {
-            /*GetLocalisation malocalisation = new GetLocalisation(getApplicationContext());
+            GetLocalisation malocalisation = new GetLocalisation(getApplicationContext());
 
             LatLng quelquepart = new LatLng(malocalisation.latitude, malocalisation.longitude);
             LatLng ailleur = new LatLng(12.80, 3.50);
             mMap.addMarker(new MarkerOptions().position(quelquepart).title("Le beau marqueur"));
             mMap.addMarker(new MarkerOptions().position(ailleur).title("Le second marquer"));
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(quelquepart));*/
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(quelquepart));
             Log.d(TestMap.class.getName(), "Je suis dans le receiver");
         }
     }
